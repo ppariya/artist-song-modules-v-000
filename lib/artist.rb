a885f08b2fd9b0afa0ca35a5861d8b4167333d53
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-05-29 05:49:06 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,11 @@
 require 'pry'
 
 class Artist
-  extend Memorable
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
+  
   attr_accessor :name
   attr_reader :songs
 
